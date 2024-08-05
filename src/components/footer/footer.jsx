a58e2a3fc95dc07517fcc3269dd139b4fdcea2e0
Lerdempp <erdemkandemir35@gmail.com>
2024-08-05 07:30:18 +0300

    Update footer.jsx

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const footer = () => {
+  return (
+    <footer className='bg-dark text-white mt-4 p-4 text-center'>
+      &copy; {new Date().getFullYear()} Lerdempp. Thats it.
+    </footer>
+  )
+}
+
+export default footer
